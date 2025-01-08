2faaca1cd02c023a1e2db4f8ef2acb430cc351ba
tomo960714 <90306200+tomo960714@users.noreply.github.com>
2025-01-08 04:13:23 +0100

    added CardRank and Card suit as Enum and made it testable

--- classes.py
+++ classes.py
@@ -1,49 +1,75 @@
 from typing import List
 
+from enum import Enum
+
+##{"Hearts", "Diamonds", "Clubs", "Spades"}
+class CardSuit(Enum):
+    HEARTS = "H"
+    DIAMONDS = "D"
+    CLUBS = "C"
+    SPADES = "S"
+
+CardRank = Enum("CardRank", {"A": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6":6,"7":7,"8":8,"9":9,"10":10,"J":11,"Q":12,"K":13})
+
+
 
 class CardClass:
     """ 
     Class to uniquely identify cards
 
     Args:
-    color: "Hearts" | "Diamonds" | "Clubs" | "Spades"
+    suit: Suits : "Hearts" | "Diamonds" | "Clubs" | "Spades"
     number: numeric representaion of the cards (1-13)
     AB_deck: distinction between A and B decks
-    symbol: symbolic representaion of the number (A | 1-10 | J | Q | K)
     name: representaion of the cards color,symbol and AB_deck (eg. Hearts Ace A -> HAA)
      """
 
-    def __init__(self,color: str,number: int, AB_deck: str):
-        valid_colors = {"Hearts", "Diamonds", "Clubs", "Spades"}
-        if number < 1 or number > 13:
-            raise ValueError("Number must be between 1 and 13")
-        if color not in valid_colors:
-            raise ValueError(f"Color must be on of: Hearts, Diamonds, Clubs or Spades and given color is {color}")
-        """ if AB_deck != "A" or AB_deck != "B":
-            raise ValueError(f"AB_deck has to be A or B ") """
+    def __init__(self,suit:str,rank:int,AB_deck:str):
+        
+        if not self._is_valid_card_suit(suit):
+            raise ValueError(f"Invalid card rank: {rank}. Must be between 1 (Ace) and 13 (King).")
+        if not self._is_valid_card_number(rank):
+            raise ValueError(f"Invalid card rank: {rank}. Must be between 1 (Ace) and 13 (King).")
+        if not self._is_deck_valid(AB_deck):
+            raise ValueError(f"Invalid value: {AB_deck}. Must be 'A' or 'B'.")
+
         
         
-        self.color = color
-        self.number = number
+        self.suit = CardSuit(suit)
+        self.rank = CardRank(rank)
         self.AB_deck = AB_deck.upper()
-        self.symbol = self._get_symbol(number)
         self.name = self._set_name()
 
-    # Match the nr to the card symbol
-    def _get_symbol(self,number:int) -> str :
-        if number == 1:
-            return "A"
-        elif number == 11:
-            return "J"
-        elif number == 12:
-            return "Q"
-        elif number == 13:
-            return "K"
-        else:
-            return str(number)
+    
+    @staticmethod
+    def _is_valid_card_number(value:int) -> bool :
+        """Internal method to check if a rank is valid."""
+        try:
+            CardRank(value)
+            return True
+        except ValueError:
+            return False
         
-    def _set_name(self):
-        return self.color[0] + self.symbol + self.AB_deck
+    @staticmethod
+    def _is_valid_card_suit(value:str) -> bool:
+        """Internal method to check if a suit is valid."""
+        try:
+            CardSuit(value)
+            return True
+        except ValueError:
+            return False
+    
+    @staticmethod
+    def _is_deck_valid(value:str) -> bool:
+        """Internal method to check if the deck is valid."""
+        try:
+            if value in ("A","B"):
+                return True
+        except ValueError:
+            return False
+
+    def _set_name(self) -> str:
+        return self.suit.value + str(self.rank.name) + self.AB_deck
     
 class StackClass:
     def __init__(self,position: int):
